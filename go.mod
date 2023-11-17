98136585a1ccd23b8486ba119fcf65f0e51fe55a
Sangwoo Han <sangw2ohan@gmail.com>
2023-11-17 09:49:10 -0500

    setup

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/therealsangwoohan/greetings
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/therealsangwoohan/greetings/hello"
+)
+
+func main() {
+	fmt.Println(hello.Hello("Sangwoo"))
+}
